2a600ba5340c8cfcafd6a06bf0b0f490758334ea
RohanSakhuja <rohansakhuja0@gmail.com>
2018-08-19 23:30:40 +0530

    Testing

--- #1 Telegram bot/scrape.py
+++ #1 Telegram bot/scrape.py
@@ -1,11 +1,8 @@
 from bs4 import BeautifulSoup
+import random
 import requests
 import json
 import os
-
-# Check if memes folder exists
-if not os.path.exists(os.path.join(os.getcwd(), 'memes')):
-	os.makedirs(os.path.join(os.getcwd(), 'memes'))
 
 # Request headers to avoid 403 Forbidden
 HEADERS = {
@@ -14,14 +11,10 @@ HEADERS = {
 
 link = 'https://knowyourmeme.com/memes/memes/photos/trending'
 
-# Meme limit to download
-limit = 1
-
 page_source = requests.get(link, headers=HEADERS).text
 soup = BeautifulSoup(page_source, 'html.parser')
 
 gallery = soup.find('div', {'id': 'photo_gallery'})
 images = gallery.find_all('img')
 
-for image in images[:limit]:
-	image_link = image['data-src']
+image_link = images[random.randint(1,len(images))]['data-src']
