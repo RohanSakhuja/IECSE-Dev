c5436718a3a2857cb2281e1ea24a17cc61e8a41d
Steve Roche <1steveroche@gmail.com>
2018-08-19 19:43:18 +0530

    HD Meme Support

--- Scraper/scrape.py
+++ Scraper/scrape.py
@@ -13,6 +13,7 @@ HEADERS = {
 link = 'https://knowyourmeme.com/memes/memes/photos/trending'
 
 limit = 3
+high_def = True
 
 page_source = requests.get(link, headers=HEADERS).text
 soup = BeautifulSoup(page_source, 'html.parser')
@@ -25,12 +26,15 @@ meme_details = []
 for image in images[:limit]:
 	image_link = image['data-src']
 	image_desc = image['alt']
-	file_name = image_link.split('/')[-1:][0]
+	image_identifier = '/'.join(image_link.split('/')[-4:])
+	file_name = image_link.split('/')[-1]
+
+	if high_def == True:
+		image_link = 'https://i.kym-cdn.com/photos/images/newsfeed/{id}'.format(id=image_identifier)
 
 	r = requests.get(image_link)
-	print(image_link)
+	print('Downloading: {link}'.format(link=image_link))
 
-	print(file_name)
 	with open('memes/{name}'.format(name=file_name), 'wb') as f:
 		f.write(r.content)
 
